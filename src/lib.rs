fdc3395d244d129de3d5a58b50ee85f073b601bc
Christian Meusel <christian.meusel@posteo.de>
2021-12-19 13:39:07 +0100

    Add remaining mono fonts from e-g 0.6.2
    The 6x6 font is an example of a variable-width bitmap font which were
    supported back then. Therefor it is left-out here in the first place.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@ use embedded_graphics::{
 
 const GLYPH_MAPPING: StrGlyphMapping =
     StrGlyphMapping::new("\0\u{20}\u{7e}\0\u{a1}\u{ff}", '?' as usize - ' ' as usize);
+const GLYPH_MAPPING_6X12: StrGlyphMapping =
+    StrGlyphMapping::new("\0\u{20}\u{7e}", '?' as usize - ' ' as usize);
 
 
 pub const FONT_24X32: MonoFont = MonoFont {
@@ -30,5 +32,35 @@ pub const FONT_12X16: MonoFont = MonoFont {
     baseline: 13,
     strikethrough: DecorationDimensions::new(7, 2),
     underline: DecorationDimensions::new(15, 2),
+    glyph_mapping: &GLYPH_MAPPING,
+};
+
+pub const FONT_8X12: MonoFont = MonoFont {
+    image: ImageRaw::new_binary(include_bytes!("../data/font8x16_1bpp.raw"), 240),
+    character_size: Size::new(8, 16),
+    character_spacing: 0,
+    baseline: 11,
+    strikethrough: DecorationDimensions::new(6, 2),
+    underline: DecorationDimensions::new(13, 2),
+    glyph_mapping: &GLYPH_MAPPING,
+};
+
+pub const FONT_6X12: MonoFont = MonoFont {
+    image: ImageRaw::new_binary(include_bytes!("../data/font6x12_1bpp.raw"), 96),
+    character_size: Size::new(6, 12),
+    character_spacing: 0,
+    baseline: 9,
+    strikethrough: DecorationDimensions::new(5, 1),
+    underline: DecorationDimensions::new(11, 1),
+    glyph_mapping: &GLYPH_MAPPING_6X12,
+};
+
+pub const FONT_6X8: MonoFont = MonoFont {
+    image: ImageRaw::new_binary(include_bytes!("../data/font6x8_1bpp.raw"), 240),
+    character_size: Size::new(6, 8),
+    character_spacing: 0,
+    baseline: 6,
+    strikethrough: DecorationDimensions::new(3, 1),
+    underline: DecorationDimensions::new(8, 1),
     glyph_mapping: &GLYPH_MAPPING,
 };
